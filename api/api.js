00fd2989756dc22c938a5ecbca24744191fe9a14
SamCo <admin@samcomods.co.uk>
2021-01-16 17:00:25 +0000

    Updated admin area.
    Added add resources as well as having the set resources.

--- api/api.js
+++ api/api.js
@@ -161,6 +161,78 @@ module.exports.load = async function(app, db) {
     }
   });  
 
+  app.post("/api/addresources", async (req, res) => {
+    let settings = await check(req, res);
+    if (!settings) return;
+
+    if (!req.body) return res.send({status: "missing body"});
+
+    if (typeof req.body.id !== "string") return res.send({status: "missing id"});
+
+    if (!(await db.get("users-" + req.body.id))) res.send({status: "invalid id"});
+
+    if (typeof req.body.ram == "number" || typeof req.body.disk == "number" || typeof req.body.cpu == "number" || typeof req.body.servers == "number") {
+      let ram = req.body.ram;
+      let disk = req.body.disk;
+      let cpu = req.body.cpu;
+      let servers = req.body.servers;
+
+      let currentextra = await db.get("extra-" + req.body.id);
+      let extra;
+
+      if (typeof currentextra == "object") {
+        extra = currentextra;
+      } else {
+        extra = {
+          ram: 0,
+          disk: 0,
+          cpu: 0,
+          servers: 0
+        }
+      }
+
+      if (typeof ram == "number") {
+        if (ram < 0 || ram > 999999999999999) {
+          return res.send({status: "ram size"});
+        }
+        extra.ram = extra.ram + ram;
+      }
+      
+      if (typeof disk == "number") {
+        if (disk < 0 || disk > 999999999999999) {
+          return res.send({status: "disk size"});
+        }
+        extra.disk = extra.disk + disk;
+      }
+      
+      if (typeof cpu == "number") {
+        if (cpu < 0 || cpu > 999999999999999) {
+          return res.send({status: "cpu size"});
+        }
+        extra.cpu = extra.cpu + cpu;
+      }
+
+      if (typeof servers == "number") {
+        if (servers < 0 || servers > 999999999999999) {
+          return res.send({status: "server size"});
+        }
+        extra.servers = extra.servers + servers;
+      }
+      
+      if (extra.ram == 0 && extra.disk == 0 && extra.cpu == 0 && extra.servers == 0) {
+        res.send({status: "invalid amount"});
+      } else {
+        await db.set("extra-" + req.body.id, extra);
+      }
+
+      adminjs.suspend(req.body.id);
+      return res.send({status: "success"});
+    } else {
+      res.send({status: "missing variables"});
+    }
+  });  
+
+
   async function check(req, res) {
     let settings = JSON.parse(fs.readFileSync("./settings.json").toString());
     if (settings.api.client.api.enabled == true) {
