ef5b1e75603314a8beddef0860432f7e735a014a
Two <njsolomonoh@gmail.com>
2021-02-11 19:14:34 -0500

    🐛Fixed /api/setcoins.

--- api/api.js
+++ api/api.js
@@ -86,20 +86,21 @@ module.exports.load = async function(app, db) {
     let coins = req.body.coins;
 
     if (typeof id !== "string") return res.send({status: "id must be a string"});
-    if (!(await db.get("users-" + id))) return res.redirect({status: "invalid id"});
+
+    if (!(await db.get("users-" + id))) return res.send({status: "invalid id"});
 
     if (typeof coins !== "number") return res.send({status: "coins must be number"});
 
-    if (coins < 0 || coins > 999999999999999) return res.redirect({status: "too small or big coins"});
+    if (coins < 0 || coins > 999999999999999) return res.send({status: "too small or big coins"});
 
     if (coins == 0) {
-        await db.delete("coins-" + id)
+      await db.delete("coins-" + id)
     } else {
-        await db.set("coins-" + id, coins);
+      await db.set("coins-" + id, coins);
     }
 
-    res.redirect({status: "success"});
-});
+    res.send({status: "success"});
+  });
 
   app.post("/api/setplan", async (req, res) => {
     let settings = await check(req, res);
